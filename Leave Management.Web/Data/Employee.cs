92a24774318b3b3ae04b021f372e5928dbf0c7f7
Dave Chapman <dave@allmyit.co.nz>
2022-04-21 10:39:47 +1200

    Add Database Tables for Leave Allocation and Leave Types

---
+++ Leave Management.Web/Data/Employee.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Leave_Management.Web.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string? TaxId { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+
+
+    }
+}
---
+++ Leave Management.Web/Data/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿namespace Leave_Management.Web.Data
+{
+    public class LeaveType : BaseEntity
+    {
+
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+
+    }
+}
