30ac0bfb687f5eee6be47dcbf9c012b0c9f87bab
Dave Chapman <dave@allmyit.co.nz>
2022-04-21 12:25:00 +1200

    Episode 17 finish

---
+++ Leave Management.Web/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Leave_Management.Web.Data;
+using Leave_Management.Web.Models;
+
+namespace Leave_Management.Web.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
---
+++ Leave Management.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Leave_Management.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+
+        [Display(Name = "Default Number of Days")]
+        [Required]
+        [Range(0,15, ErrorMessage ="Please enter a valid number")]
+        public int DefaultDays { get; set; }
+    }
+}
